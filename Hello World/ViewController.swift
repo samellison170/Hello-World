a2af6c14ecc98ae8de749494f4f3f0afb474287e
Ellison <spevy3@mail.missouri.edu>
2018-08-24 17:37:01 -0500

    First commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Ellison, Samuel P. (MU-Student) on 8/24/18.
+//  Copyright © 2018 Ellison, Samuel P. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
